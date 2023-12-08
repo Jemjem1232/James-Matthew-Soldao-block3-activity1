c49cade8367f59a7e5874b608666b810ad29d779
admin <jama.soldao.up@phinamed.com>
2023-12-08 15:36:54 +0800

    button linked

--- app/src/main/java/Soldao_JamesMatthew_Block3/p1quiz/MainActivity.kt
+++ app/src/main/java/Soldao_JamesMatthew_Block3/p1quiz/MainActivity.kt
@@ -1,11 +1,25 @@
 package Soldao_JamesMatthew_Block3.p1quiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
-class MainActivity : AppCompatActivity() {
+class cardpicker : AppCompatActivity() {
+
+    lateinit var imageView: ImageView
+    lateinit var button: Button
+
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+
     }
 }
